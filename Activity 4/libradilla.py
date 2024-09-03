941ee5dc62aab8b01ca1679ea03a46786d2b22e4
milvss <libradillamilva0@gmail.com>
2024-09-03 10:49:43 +0800

    Update libradilla.py

--- Activity 4/libradilla.py
+++ Activity 4/libradilla.py
@@ -3,3 +3,15 @@ sampleText2a = sampleText2. format("milva", "chicken", "billiards")
 print(sampleText2a)
 
 my name is milva i love chicken and playing billiards
+
+sampleText3 "ny name is (name). I love (food), and playing (play)."
+sampleText3a sampleText3.format(name "milva", food shushi", play football")
+print(sampleTextla)
+
+
+item "milk"
+cost 35.50
+
+
+sampleText4 "the product is Ns cost %.2f (item, cost)
+print(sampleText4)
